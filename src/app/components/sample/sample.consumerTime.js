a7ea55a8080223df00f44d03a9cd252f07095052
MarcoMunozOvalle2019e <marco.munoz.ovalle2010@gmaile.com>
2021-10-09 12:42:34 -0300

    feat: se agrega logica On

--- src/app/components/sample/sample.consumerTime.js
+++ src/app/components/sample/sample.consumerTime.js
@@ -1,8 +1,28 @@
+let reportFail = [];
 
 const proyects = require("../../../../data/data");
 
 class ExecuteSample {
   static async On(req) {
+    try {
+      const { proyectId } = req.body;
+      let project = proyects.find((element) => element.job === proyectId);
+      if (project) {
+        if (project.state === 1) {
+          project.error++;
+        } else {
+          project.beginTime = Date.now();
+          project.state = 1;
+          project.spendTime = 0;
+          project.error = 0;
+        }
+      } else {
+        project = "does no exist project";
+      }
+      return project;
+    } catch (err) {
+      return err;
+    }
   }
 
   static async Off(req) {
